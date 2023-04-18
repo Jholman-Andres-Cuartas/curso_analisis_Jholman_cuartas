b154cb9359b66dfed6e9dce0a28c22fb20e95eda
Jholman Cuartas <jacuartasm@ut.edu.co>
2023-04-18 20:04:16 +0200

    base de datos

---
+++ compani.sql
@@ -0,0 +1,5 @@
+id,first_name,nif,cargo,departamentp
+1,Alan,666666G,"Ing sistemas",tecnologia
+2,Prueba,343434Y,"recursos humanos","recursos humanos"
+3,andres,123456a,"Ing forestal",ambiental
+4,pepito,98765z,Gerente,direccion
