d21766ca94b2d9ad47ea4a044f89c2926d1b0217
Jholman-Andres-Cuartas <130378924+Jholman-Andres-Cuartas@users.noreply.github.com>
2023-05-21 14:56:25 +0200

    ejercicios funciones sql

---
+++ m1-sql/3.funciones/ejercicio3.sql
@@ -0,0 +1,6 @@
+-- Funciones cadena
+
+-- CONCAT sobre firstname y lastname de sakila.customer
+select * from customer;
+SELECT customer_id, CONCAT(first_name, ' ', last_name) as full_name, email 
+FROM sakila.customer;
